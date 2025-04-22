a7a6911510b1a46d9c462ba5cf55bc0fbfdde3a2
ruksanabetgeri <ruksanabetageri300@gmail.com>
2025-04-22 13:57:01 +0530

    save the file in main

---
+++ Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+    public static void main(String[] args) {
+        String name="Vaishnavi";
+        int usn=122;
+        int fees=80000;
+        String college="KLETECH";
+
+        System.out.println("-------STUDENT DETAILS--------");
+        System.out.println("Name:"+name);
+        System.out.println("USN:"+usn);
+        System.out.println("Fees:"+fees);
+        System.out.println("College:"+college);
+    }
+}

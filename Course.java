15bbb9d0079afdafb591f4b608a2a45220a817e5
ruksanabetgeri <ruksanabetageri300@gmail.com>
2025-04-22 15:02:41 +0530

    save in main

---
+++ Course.java
@@ -0,0 +1,12 @@
+public class Course {
+        public static void main(String[] args) {
+            String courseName = "Java Programming";
+            String instructor = "John Doe";
+            int duration = 12;
+            
+            System.out.println("Course: " + courseName);
+            System.out.println("Instructor: " + instructor);
+            System.out.println("Duration: " + duration + " weeks");
+        }
+    }
+  
